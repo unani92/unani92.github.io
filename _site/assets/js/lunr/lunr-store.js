378cf66e2cd08bb3c0105c70576ec86ebbaa82e7
unani <unani92@naver.com>
2020-05-26 00:22:17 +0900

    update javascript

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,16 @@
 var store = [{
+        "title": "Vanilla JS로 momentum 클론코딩하기 1",
+        "excerpt":"클론코딩 컨셉 화면에 큰 시계와 함께 간단하게 toDo 리스트를 작성하고 수정 및 삭제할 수 있는 앱을 vanilla JS로 클론코딩해 보았다. 변수 할당 DOM 조작을 통해 시계를 집어넣을 공간은 다음과 같다. &lt;div class=\"clock\"&gt; &lt;h1 class=\"my-5\"&gt;00:00&lt;/h1&gt; &lt;/div&gt; h1 태그 안의 내용은 더미 텍스트로 어차피 시간에 맞춰저 해당 내용은 변할 것이다. 구글 모멘텀을...","categories": ["Javascript"],
+        "tags": [],
+        "url": "http://localhost:4000/javascript/Vanilla-JS%EB%A1%9C-momentum-%ED%81%B4%EB%A1%A0%EC%BD%94%EB%94%A9%ED%95%98%EA%B8%B0-1/",
+        "teaser": "http://localhost:4000/assets/images/background.jpeg"
+      },{
+        "title": "Vanilla JS로 momentum 클론코딩하기 2",
+        "excerpt":"기본 컨셉 1. 할 일을 입력 후 enter를 친다. 2. toDo List에 업데이트된다. 3. 할일 마감 후 toDo의 글자부분을 클릭하면 Complete로 이동한다. 4. 이동된 toDo는 취소선이 그어지며, 클릭 시 다시 toDo List로 이동한다. 5. 수정 및 삭제는 ToDo List, Completed 어디에서도 가능하다. 초기 변수 선언 및 할당 const form =...","categories": ["Javascript"],
+        "tags": [],
+        "url": "http://localhost:4000/javascript/Vanilla-JS%EB%A1%9C-momentum-%ED%81%B4%EB%A1%A0%EC%BD%94%EB%94%A9%ED%95%98%EA%B8%B02/",
+        "teaser": "http://localhost:4000/assets/images/background.jpeg"
+      },{
         "title": "Axios 라이브러리를 통한 비동기처리 방식",
         "excerpt":"Index : 비동기 처리란 무엇인가? 정의 : 특정 코드의 연산이 끝날 때까지 코드의 실행을 멈추지 않고 다음 코드를 먼저 실행하는 자바스크립트의 처리 이것은 왜 필요한가? 이렇듯 특정 로직의 실행이 끝날 때까지 기다려주지 않고 나머지 코드를 먼저 실행하는 일은 브라우저에서 효율적인 작업을 위해 필요하다. 이해를 위해 일상적인 사례를 비유로 들어보자 오늘...","categories": ["Javascript"],
         "tags": [],
